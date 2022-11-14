8b401b505af4dd9b9ee4947747c330f157cce7b7
A7med-sharaf <you@example.com>
2022-11-15 00:03:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.auto', {
+    strings: [
+    'Fornt-End Developer','HTML','CSS','javaScript','office-word','office-powerpoint','github' ],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true,
+    });
+
